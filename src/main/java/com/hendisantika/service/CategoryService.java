b523f003961f997c100fa2a2306ff0518203c5e7
Hendi Santika <hendisantika@gmail.com>
2021-11-23 04:01:28 +0700

    #18 - Add getAllCategories in CategoryService

--- src/main/java/com/hendisantika/service/CategoryService.java
+++ src/main/java/com/hendisantika/service/CategoryService.java
@@ -1,8 +1,13 @@
 package com.hendisantika.service;
 
+import com.hendisantika.entity.Category;
 import com.hendisantika.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +23,9 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @Transactional
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
 }
