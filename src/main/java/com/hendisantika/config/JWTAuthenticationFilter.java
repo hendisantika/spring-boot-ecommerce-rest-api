8125113745a5b49ecced79a2f54d346fceb9a2b0
Hendi Santika <hendisantika@gmail.com>
2021-11-23 03:49:41 +0700

    #6 - Add JWTAuthenticationFilter

---
+++ src/main/java/com/hendisantika/config/JWTAuthenticationFilter.java
@@ -0,0 +1,53 @@
+package com.hendisantika.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-ecommerce-rest-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 23/11/21
+ * Time: 03.49
+ */
+@Component
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(JWTAuthenticationFilter.class);
+    private static final String HEADER_STRING = "Authorization";
+
+    @Autowired
+    private SecurityService securityService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpRequest, HttpServletResponse httpResponse,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        final String token = httpRequest.getHeader(HEADER_STRING);
+
+        if (token != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            try {
+                securityService.authenticate(token);
+            } catch (Exception e) {
+                logger.debug("Failed when authenticating token '{}'. Error: '{}'", token, e.getMessage());
+            }
+        }
+
+        filterChain.doFilter(httpRequest, httpResponse);
+
+    }
+
+}
