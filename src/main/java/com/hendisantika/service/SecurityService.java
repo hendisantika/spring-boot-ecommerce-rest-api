47c252a04a8124aa3b4e80b55a4c0b62665ec701
Hendi Santika <hendisantika@gmail.com>
2021-11-23 04:09:39 +0700

    #38 - Add authenticate in SecurityService

--- src/main/java/com/hendisantika/service/SecurityService.java
+++ src/main/java/com/hendisantika/service/SecurityService.java
@@ -1,9 +1,14 @@
 package com.hendisantika.service;
 
+import com.hendisantika.entity.User;
 import com.hendisantika.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,5 +27,18 @@ public class SecurityService {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Transactional
+    public String authenticate(final String username, final String password) {
+        final Authentication authentication =
+                authenticationManager.authenticate(
+                        new UsernamePasswordAuthenticationToken(username, password)
+                );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        final User user = this.loadUserByUsername(username);
+
+        return JwtUtils.generateToken(user.getId(), user.getUsername(), user.getRole());
+    }
 
 }
