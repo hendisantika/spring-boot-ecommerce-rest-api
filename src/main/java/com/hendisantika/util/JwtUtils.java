b42ada1123f37b07076da0c4a7ef91972dd4e1ac
Hendi Santika <hendisantika@gmail.com>
2021-11-23 04:11:25 +0700

    #43 - Add generateToken in JwtUtils

--- src/main/java/com/hendisantika/util/JwtUtils.java
+++ src/main/java/com/hendisantika/util/JwtUtils.java
@@ -1,5 +1,10 @@
 package com.hendisantika.util;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-ecommerce-rest-api
@@ -15,4 +20,22 @@ public class JwtUtils {
     private static final String TOKEN_CLAIM_USERNAME = "username";
     private static final String TOKEN_CLAIM_ROLES = "roles";
     private static final long TOKEN_EXPIRATION = 3_600_000; // 1 hour
+
+    private static String generateToken(final Long userId, final String username, final String userRole) {
+        final Date now = new Date();
+        final Date exp = new Date(now.getTime() + TOKEN_EXPIRATION);
+
+        String jwtToken =
+                Jwts.builder()
+                        .setSubject(userId.toString())
+                        .claim(TOKEN_CLAIM_USERNAME, username)
+                        .claim(TOKEN_CLAIM_ROLES, userRole)
+                        .setIssuedAt(now)
+                        .setNotBefore(now)
+                        .setExpiration(exp)
+                        .signWith(SignatureAlgorithm.HS256, TOKEN_SECRET_KEY)
+                        .compact();
+
+        return TOKEN_PREFIX + " " + jwtToken;
+    }
 }
