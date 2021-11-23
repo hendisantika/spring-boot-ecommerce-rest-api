2f3276bbe1be73f237fb8839a89b0af65b4787a4
Hendi Santika <hendisantika@gmail.com>
2021-11-24 06:38:14 +0700

    #73 - Add addProduct in CategoryProductsController

--- src/main/java/com/hendisantika/api/controller/CategoryProductsController.java
+++ src/main/java/com/hendisantika/api/controller/CategoryProductsController.java
@@ -10,9 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -53,5 +55,24 @@ public class CategoryProductsController {
         return ResponseEntity.ok(pagedResourcesAssembler.toEmptyModel(products, productResourceAssembler));
     }
 
+    @PostMapping(path = "/{productid}")
+    public ResponseEntity<?> addProduct(@PathVariable Long categoryid, @PathVariable Long productid) {
+        // Getting the requiring category; or throwing exception if not found
+        final Category category = categoryService.getCategoryById(categoryid)
+                .orElseThrow(() -> new NotFoundException("category"));
 
+        // Getting the requiring product; or throwing exception if not found
+        final Product product = productService.getProductById(productid)
+                .orElseThrow(() -> new NotFoundException("product"));
+
+        // Validating if association does not exist...
+        if (productService.hasCategory(product, category)) {
+            throw new IllegalArgumentException("product " + product.getId() + " already contains category " + category.getId());
+        }
+
+        // Associating product with category...
+        productService.addCategory(product, category);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(productResourceAssembler.toModel(product));
+    }
 }
