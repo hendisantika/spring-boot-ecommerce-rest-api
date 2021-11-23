4d2db05c5e827c55dc738a964ac0021a464317d2
Hendi Santika <hendisantika@gmail.com>
2021-11-23 08:30:55 +0700

    #48 - Add ProductResource

---
+++ src/main/java/com/hendisantika/api/resource/ProductResource.java
@@ -0,0 +1,27 @@
+package com.hendisantika.api.resource;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-ecommerce-rest-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 23/11/21
+ * Time: 08.29
+ */
+@Getter
+@AllArgsConstructor
+public class ProductResource extends RepresentationModel {
+
+    private final String name;
+    private final String currency;
+    private final double price;
+    private final List<CategoryResource> categories;
+    private final String owner;
+}
