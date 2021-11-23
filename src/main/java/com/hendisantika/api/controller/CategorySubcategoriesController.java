e43577747e7ca5a79b04ed7b3466e16c400ff93b
Hendi Santika <hendisantika@gmail.com>
2021-11-24 06:45:49 +0700

    #76 - Add retrieveAllSubcategories in CategorySubcategoriesController

--- src/main/java/com/hendisantika/api/controller/CategorySubcategoriesController.java
+++ src/main/java/com/hendisantika/api/controller/CategorySubcategoriesController.java
@@ -1,10 +1,17 @@
 package com.hendisantika.api.controller;
 
 import com.hendisantika.api.assembler.CategoryResourceAssembler;
+import com.hendisantika.entity.Category;
+import com.hendisantika.exception.NotFoundException;
 import com.hendisantika.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +31,16 @@ public class CategorySubcategoriesController {
 
     @Autowired
     private CategoryResourceAssembler categoryResourceAssembler;
+
+    @GetMapping
+    public ResponseEntity<?> retrieveAllSubcategories(@PathVariable Long parentid) {
+        // Getting the requiring category; or throwing exception if not found
+        final Category parent = categoryService.getCategoryById(parentid)
+                .orElseThrow(() -> new NotFoundException("parent category"));
+
+        // Getting all categories in application...
+        final Set<Category> subcategories = parent.getChildCategories();
+
+        return ResponseEntity.ok(categoryResourceAssembler.toModel(subcategories));
+    }
 }
