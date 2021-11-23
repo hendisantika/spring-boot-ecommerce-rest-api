30caa2bcc8b5019128ba966eaa293487f0457103
Hendi Santika <hendisantika@gmail.com>
2021-11-24 06:21:44 +0700

    #67 - Add createProduct in ProductController

--- src/main/java/com/hendisantika/api/controller/ProductController.java
+++ src/main/java/com/hendisantika/api/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.hendisantika.api.controller;
 
 import com.hendisantika.api.assembler.ProductResourceAssembler;
 import com.hendisantika.entity.Product;
+import com.hendisantika.entity.User;
 import com.hendisantika.exception.NotFoundException;
 import com.hendisantika.service.ProductService;
 import com.hendisantika.service.SecurityService;
@@ -9,11 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * Created by IntelliJ IDEA.
@@ -57,4 +63,14 @@ public class ProductController {
         return ResponseEntity.ok(productResourceAssembler.toResource(product));
     }
 
+    @PostMapping
+    public ResponseEntity<?> createProduct(@RequestBody @Valid ProductDto request) {
+        // Gets the current logged-in User...
+        final User user = securityService.getCurrentUser();
+        // Creating a new product in the application...
+        final Product product = productService.createProduct(request.getName(), request.getCurrency(),
+                request.getPrice(), user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(productResourceAssembler.toResource(product));
+    }
 }
