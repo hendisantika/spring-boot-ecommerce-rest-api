7ede498cc6d2d61576e89203f7258096ee945a8a
Hendi Santika <hendisantika@gmail.com>
2021-11-24 06:37:37 +0700

    #72 - Add retrieveAllProducts in CategoryProductsController

--- src/main/java/com/hendisantika/api/controller/CategoryProductsController.java
+++ src/main/java/com/hendisantika/api/controller/CategoryProductsController.java
@@ -1,10 +1,18 @@
 package com.hendisantika.api.controller;
 
+import com.hendisantika.api.assembler.ProductResourceAssembler;
+import com.hendisantika.entity.Category;
 import com.hendisantika.entity.Product;
+import com.hendisantika.exception.NotFoundException;
 import com.hendisantika.service.CategoryService;
 import com.hendisantika.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +40,18 @@ public class CategoryProductsController {
 
     @Autowired
     private PagedResourcesAssembler<Product> pagedResourcesAssembler;
+
+    @GetMapping
+    public ResponseEntity<?> retrieveAllProducts(@PathVariable Long categoryid, Pageable pageable) {
+        // Getting the requiring category; or throwing exception if not found
+        final Category category = categoryService.getCategoryById(categoryid)
+                .orElseThrow(() -> new NotFoundException("category"));
+
+        // Getting all products in application...
+        final Page<Product> products = productService.getAllProducts(category, pageable);
+
+        return ResponseEntity.ok(pagedResourcesAssembler.toEmptyModel(products, productResourceAssembler));
+    }
+
 
 }
