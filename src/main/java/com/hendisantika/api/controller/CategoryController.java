02e7e74c27a994ff2f1ab4610636c7e7050546ed
Hendi Santika <hendisantika@gmail.com>
2021-11-23 08:44:36 +0700

    #58 - Add updateCategory in CategoryController

--- src/main/java/com/hendisantika/api/controller/CategoryController.java
+++ src/main/java/com/hendisantika/api/controller/CategoryController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -60,5 +61,17 @@ public class CategoryController {
         final Category category = categoryService.createCategory(request.getName());
 
         return ResponseEntity.status(HttpStatus.CREATED).body(categoryResourceAssembler.toResource(category));
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<?> updateCategory(@PathVariable Long id, @RequestBody @Valid CategoryDto request) {
+        // Getting the requiring category; or throwing exception if not found
+        final Category category = categoryService.getCategoryById(id)
+                .orElseThrow(() -> new NotFoundException("category"));
+
+        // Updating a category in the application...
+        categoryService.updateCategory(category, request.getName());
+
+        return ResponseEntity.ok(categoryResourceAssembler.toResource(category));
     }
 }
