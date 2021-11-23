ca5df197614c2d8ef6d8e4c9595865e3a932baec
Hendi Santika <hendisantika@gmail.com>
2021-11-23 08:56:02 +0700

    #60 - Add CategoryResourceAssembler

--- src/main/java/com/hendisantika/api/assembler/CategoryResourceAssembler.java
+++ src/main/java/com/hendisantika/api/assembler/CategoryResourceAssembler.java
@@ -1,10 +1,12 @@
 package com.hendisantika.api.assembler;
 
+import com.hendisantika.api.controller.CategoryController;
 import com.hendisantika.api.resource.CategoryResource;
 import com.hendisantika.entity.Category;
 import com.hendisantika.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Component;
 
 /**
@@ -26,4 +28,24 @@ public class CategoryResourceAssembler extends RepresentationModelAssemblerSuppo
         super(CategoryController.class, CategoryResource.class);
     }
 
+    @Override
+    protected CategoryResource instantiateResource(Category entity) {
+        return new CategoryResource(entity.getName());
+    }
+
+    @Override
+    public CategoryResource toResource(Category entity) {
+        CategoryResource resource = createResourceWithId(entity.getId(), entity);
+        if (entity.getParent() != null) {
+            resource.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CategoryController.class).retrieveCategory(entity.getParent().getId())).withRel("parent"));
+        }
+        if (entity.getChildCategories() != null) {
+            resource.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CategorySubcategoriesController.class).retrieveAllSubcategories(entity.getId())).withRel("subcategories"));
+        }
+        if (productService.hasProductsAssociated(entity)) {
+            resource.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CategoryProductsController.class).retrieveAllProducts(entity.getId(), null)).withRel("products"));
+        }
+
+        return resource;
+    }
 }
