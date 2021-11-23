94f484673d88c0c14e0fb066af25629031d91339
Hendi Santika <hendisantika@gmail.com>
2021-11-23 08:24:34 +0700

    #45 - Add JwtUtils

--- src/main/java/com/hendisantika/service/SecurityService.java
+++ src/main/java/com/hendisantika/service/SecurityService.java
@@ -3,6 +3,8 @@ package com.hendisantika.service;
 import com.hendisantika.entity.User;
 import com.hendisantika.repository.UserRepository;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,6 +14,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 /**
  * Created by IntelliJ IDEA.
@@ -68,5 +72,38 @@ public class SecurityService {
     public User loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Invalid username or password."));
+    }
+
+    private static class JwtUtils {
+        private static final String TOKEN_SECRET_KEY = "ECommerceAppSecretKey";
+        private static final String TOKEN_PREFIX = "Bearer";
+        private static final String TOKEN_CLAIM_USERNAME = "username";
+        private static final String TOKEN_CLAIM_ROLES = "roles";
+        private static final long TOKEN_EXPIRATION = 3_600_000; // 1 hour
+
+        private static String generateToken(final Long userId, final String username, final String userRole) {
+            final Date now = new Date();
+            final Date exp = new Date(now.getTime() + TOKEN_EXPIRATION);
+
+            String jwtToken =
+                    Jwts.builder()
+                            .setSubject(userId.toString())
+                            .claim(TOKEN_CLAIM_USERNAME, username)
+                            .claim(TOKEN_CLAIM_ROLES, userRole)
+                            .setIssuedAt(now)
+                            .setNotBefore(now)
+                            .setExpiration(exp)
+                            .signWith(SignatureAlgorithm.HS256, TOKEN_SECRET_KEY)
+                            .compact();
+
+            return TOKEN_PREFIX + " " + jwtToken;
+        }
+
+        private static Claims parseToken(String token) {
+            return Jwts.parser()
+                    .setSigningKey(TOKEN_SECRET_KEY)
+                    .parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
+                    .getBody();
+        }
     }
 }
