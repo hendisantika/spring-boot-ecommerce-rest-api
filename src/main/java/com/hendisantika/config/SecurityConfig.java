95f8b149c5c6c0f57cf41a537cc4a06d4a64594d
Hendi Santika <hendisantika@gmail.com>
2021-11-24 07:08:11 +0700

    #81 - Add configure in SecurityConfig

--- src/main/java/com/hendisantika/config/SecurityConfig.java
+++ src/main/java/com/hendisantika/config/SecurityConfig.java
@@ -2,10 +2,15 @@ package com.hendisantika.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,4 +32,39 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder security) throws Exception {
+        security
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 1) Enabling Frames because it's required for h2. Note: this shouldn't be exposed to prod.
+        // 2) Setting exception handler in Filters
+        // 3) Allowing the following actions without authentication:
+        // 3.a) Processing OPTIONS for all endpoints
+        // 3.b) Processing POST on /login
+        // 3.c) Accessing to h2 web console. Note: this shouldn't be exposed to prod.
+        // 4) All the other requests need to be authenticated
+        // 5) Adding to the filter chain the JWT Authentication Filter
+        http
+                .csrf()
+                .disable()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .antMatchers("/h2", "/h2/**", "/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
 }
